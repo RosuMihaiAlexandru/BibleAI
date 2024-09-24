aa6f96d5d5f2258e710f39c3da7b0037db486526
Mihai <malexandru.rosu@gmail.com>
2024-09-24 13:05:35 +0300

    remove config

--- next.config.mjs
+++ next.config.mjs
@@ -11,43 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  poweredByHeader: false,
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // Ensure that all imports of 'yjs' resolve to the same instance
-      config.resolve.alias["yjs"] = path.resolve(
-        process.cwd(),
-        "node_modules/yjs"
-      );
-
-      config.node = {
-        __dirname: false,
-        __filename: false,
-      };
-    }
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            dimensions: false,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: "/bCccDwkKkN",
-        destination: "/", // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
